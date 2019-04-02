629dc53aad08b3f176a218215f390f9f89c303c9
Ali Dikici <aliriza.dikici@gmail.com>
2019-04-02 22:48:14 +0400

    Working on list all users

--- functions/src/https/preview-mail-template.ts
+++ functions/src/https/preview-mail-template.ts
@@ -1,21 +1,41 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
-// import * as cors from 'cors';
+import * as Handlebars from 'handlebars';
 
 admin.initializeApp( functions.config().firebase );
 
 const db = admin.firestore();
 // const corsHandler = cors( { origin: true } );
 
-export const previewMailTemplate = functions.https.onRequest( ( req, res ) => {
+export const previewMailTemplate = functions.https.onRequest( async ( req, res ) => {
 	// corsHandler( req, res, () => {
-	db.doc( 'mailtemplates/' + req.query.id ).get().then( ( result ) => {
-		res.set( 'Access-Control-Allow-Origin', '*' );
-		res.status( 200 ).send( ( result.data() as any ).content );
-	} ).catch( reason => {
-		console.error( 'Hey you catch' );
-		console.error( reason );
-		res.status( 500 ).end();
-	} );
+
+	res.set( 'Access-Control-Allow-Origin', '*' );
+
+	const settings = ( await db.doc( 'settings/general' ).get() ).data();
+	const template = ( ( await db.doc( 'mailtemplates/' + req.query.id ).get() ).data() as any );
+	const templateContent = template.content;
+	const templateData = ( await db.doc( template.boundCollection + '/' + template.boundDocument ).get() ).data();
+	const templateBase = Handlebars.compile( templateContent );
+	const body = templateBase( { ...templateData, settings } );
+
+	res.status( 200 ).send( body );
+
+	// const template = Handlebars.compile( templateContent );
+	// const body = template( { settings } );
+
+	// db.doc( 'mailtemplates/' + req.query.id ).get().then( ( result ) => {
+	// 	const source = ( result.data() as any ).content;
+
+	// 	console.log( source );
+	// 	console.log( body );
+
+	// 	res.status( 200 ).send( body + source );
+	// } ).catch( reason => {
+	// 	console.error( 'Hey you catch' );
+	// 	console.error( reason );
+	// 	res.status( 500 ).end();
+	// } );
+
 	// } );
 } );
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,1 +1,2 @@
 export { previewMailTemplate } from './https/preview-mail-template';
+export { listAllUsers } from './https/list-all-users';
